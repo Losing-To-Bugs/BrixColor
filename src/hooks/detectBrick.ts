fb4cd848a972595e2467f580e06c8be744847141
lh <pairofkings30@gmail.com>
2024-10-05 23:13:03 -0500

    Update LegoDetectorFrameProcessor.swift

---
+++ src/hooks/detectBrick.ts
@@ -0,0 +1,14 @@
+import { VisionCameraProxy, Frame } from 'react-native-vision-camera'
+
+const plugin = VisionCameraProxy.initFrameProcessorPlugin('detectBrick', {})
+
+export function detectBrick(frame: Frame) {
+    'worklet'
+    if (plugin == null) {
+        throw new Error("Failed to load Frame Processor Plugin!")
+    }
+    const result = plugin.call(frame);
+    console.log(plugin, result)
+
+    return result
+}
