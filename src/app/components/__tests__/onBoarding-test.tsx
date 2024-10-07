96fe04f627f8e23399e74f95aac75e253a1a1d93
Joshua Mackie <jmack2415@gmail.com>
2024-10-07 01:55:10 -0500

    Finished adding images and finished testing.

--- src/app/components/__tests__/onBoarding-test.tsx
+++ src/app/components/__tests__/onBoarding-test.tsx
@@ -2,43 +2,100 @@ import { render, fireEvent, waitFor } from "@testing-library/react-native";
 import OnboardingScreen from "@/app/onboard"; // Adjust the path as necessary
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
+import { useSettings } from "@/components/SettingsContext";
 
 jest.mock("expo-router", () => ({
   useRouter: jest.fn(),
 }));
 
+// Mocking useSettings
+jest.mock("@/components/SettingsContext", () => ({
+  useSettings: jest.fn(),
+  SettingsProvider: ({ children }) => <>{children}</>,
+}));
+
+// Mocking AsyncStorage methods
 jest.mock("@react-native-async-storage/async-storage", () => ({
   setItem: jest.fn(),
   getItem: jest.fn(),
   removeItem: jest.fn(),
 }));
-jest.mock("react-native-onboarding-swiper");
+
+// Mocking the onboarding swiper
+jest.mock("react-native-onboarding-swiper", () => {
+  const { View, Text, Button } = require("react-native");
+  return jest
+    .fn()
+    .mockImplementation(({ NextButtonComponent, pages, onDone }) => {
+      return (
+        <>
+          {pages.map((page, index) => (
+            <View key={index}>
+              <Text>{page.title}</Text>
+              <NextButtonComponent pageIndex={index} />
+            </View>
+          ))}
+          <Button title="Done" onPress={onDone} testID="done-button" />
+        </>
+      );
+    });
+});
 
 describe("OnboardingScreen", () => {
   const mockDismiss = jest.fn();
 
   beforeEach(() => {
     useRouter.mockReturnValue({ dismiss: mockDismiss });
+    (useSettings as jest.Mock).mockReturnValue({
+      theme: "Light", // Mocking a light theme
+      fontSize: "Medium", // Mocking medium font size
+      themes: {
+        Light: {
+          switchOffColor: "#E0E0E0",
+          primaryColor: "#0055BF",
+        },
+      },
+      fontSizes: {
+        Small: { fontSize: 14 },
+        Medium: { fontSize: 16 },
+        Big: { fontSize: 18 },
+        Huge: { fontSize: 20 },
+      },
+    });
     jest.clearAllMocks();
   });
 
-  it("renders onboarding pages and navigates to the second page", async () => {
-    const { getByText } = render(<OnboardingScreen />);
-    expect(getByText("Welcome TEST!")).toBeTruthy();
+  it("renders onboarding pages and navigates thru all", async () => {
+    const { getByTestId, findByText } = render(<OnboardingScreen />);
 
-    fireEvent.press(getByText("Next"));
+    // First page
+    const welcomeText = await findByText("Welcome to BrixColor!");
+    expect(welcomeText).toBeTruthy();
 
-    await waitFor(() => {
-      expect(getByText("TEST!")).toBeTruthy();
-    });
-  });
+    fireEvent.press(getByTestId("next-button-0"));
 
-  it("completes onboarding and dismisses when done", async () => {
-    const { getByText } = render(<OnboardingScreen />);
+    // Second page
+    const page2Text = await findByText(
+      "Place LEGO brick in the highlighted area."
+    );
+    expect(page2Text).toBeTruthy();
 
-    fireEvent.press(getByText("Next"));
-    fireEvent.press(getByText("Done"));
+    fireEvent.press(getByTestId("next-button-1"));
 
+    // Third page
+    const page3Text = await findByText("Once in view, tap the capture button!");
+    expect(page3Text).toBeTruthy();
+
+    fireEvent.press(getByTestId("next-button-2"));
+
+    // Final page
+    const finalText = await findByText("Thanks for using BrixColor!");
+    expect(finalText).toBeTruthy();
+
+    const doneButton = await getByTestId("done-button");
+    fireEvent.press(doneButton);
+
+    // Test if AsyncStorage.setItem is working correctly
     await waitFor(() => {
       expect(AsyncStorage.setItem).toHaveBeenCalledWith("isOnboarded", "true");
       expect(mockDismiss).toHaveBeenCalled();
--- src/app/onboard.tsx
+++ src/app/onboard.tsx
@@ -1,7 +1,8 @@
-import { Button, StyleSheet, Text } from "react-native";
+import { Button, StyleSheet, Text, Image } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SettingsProvider, useSettings } from "@/components/SettingsContext";
 
 const OnboardingScreen = () => {
   const router = useRouter();
@@ -15,37 +16,152 @@ const OnboardingScreen = () => {
     router.dismiss();
   };
 
-
-  const Next = ({ ...props }) => <Button title={"Next"} {...props} />;
-  const Done = ({ ...props }) => <Button title={"Done"} {...props} />;
+  //This convoluted button is for testing purposes.
+  const Next = ({ pageIndex, ...props }) => {
+    const currentPageIndex = pageIndex !== undefined ? pageIndex : 0;
+    return (
+      <Button
+        title={"Next"}
+        {...props}
+        testID={`next-button-${currentPageIndex}`}
+      />
+    );
+  };
+  const { theme, themes, fontSizes, fontSize } = useSettings();
+  //const Next = ({ ...props }) => <Button title={"Next"} {...props} />; simple button
+  const Done = ({ ...props }) => (
+    <Button title={"Done"} {...props} testID="done-button" />
+  );
   return (
     <Onboarding
       onDone={onDone}
+      onSkip={onDone} //should just close the onBoarding
       NextButtonComponent={Next}
       DoneButtonComponent={Done}
       pages={[
         {
-          backgroundColor: "#fff",
-          image: <Text style={styles.image}>👋</Text>,
-          title: "Welcome TEST!",
-          subtitle: "Page 1",
+          backgroundColor: themes[theme].backgroundColor,
+          image: (
+            <Image
+              source={require("../../assets/images/TutorialPage2.png")}
+              style={styles.image}
+            />
+          ),
+          title: (
+            <Text
+              style={{
+                color: themes[theme].textColor,
+                fontSize: fontSizes[fontSize].fontSize,
+                textAlign: "center",
+                margin: 15,
+              }}
+            >
+              Welcome to BrixColor!
+            </Text>
+          ),
+          subtitle: "", //need subtitles to get rid of errors.
         },
         {
-          backgroundColor: "#999",
-          image: <Text style={styles.image}>🎉</Text>,
-          title: "TEST!",
-          subtitle: "Final page",
+          backgroundColor: themes[theme].backgroundColor,
+          image: (
+            <Image
+              source={require("../../assets/images/TutorialPage2.png")}
+              style={styles.image}
+            />
+          ),
+          title: (
+            <Text
+              style={{
+                color: themes[theme].textColor,
+                fontSize: fontSizes[fontSize].fontSize,
+                textAlign: "center",
+                margin: 15,
+              }}
+            >
+              Place LEGO brick in the highlighted area.
+            </Text>
+          ),
+          subtitle: "",
+        },
+        {
+          backgroundColor: themes[theme].backgroundColor,
+          image: (
+            <Image
+              source={require("../../assets/images/TutorialPage3.gif")}
+              style={styles.image2}
+            />
+          ),
+          title: (
+            <Text
+              style={{
+                color: themes[theme].textColor,
+                fontSize: fontSizes[fontSize].fontSize,
+                textAlign: "center",
+                margin: 15,
+              }}
+            >
+              Once in view, tap the capture button!{" "}
+            </Text>
+          ),
+          subtitle: "",
+        },
+        {
+          backgroundColor: themes[theme].backgroundColor,
+          image: (
+            <Image
+              source={require("../../assets/images/GreenCheck.png")}
+              style={styles.image2}
+            />
+          ),
+          title: (
+            <Text
+              style={{
+                color: themes[theme].textColor,
+                fontSize: fontSizes[fontSize].fontSize,
+                textAlign: "center",
+                margin: 15,
+              }}
+            >
+              Thanks for using BrixColor!
+            </Text>
+          ),
+          subtitle: "",
         },
       ]}
+      imageContainerStyles={styles.imageContainer}
+      bottomBarHeight={50}
     />
   );
 };
 
 const styles = StyleSheet.create({
-  image: {
+  text: {
     fontSize: 50,
     textAlign: "center",
   },
+  imageContainer: {
+    paddingBottom: 0,
+    width: "100%",
+    height: "70%",
+    margin: 5,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  image2: {
+    width: "90%",
+    height: "90%",
+    resizeMode: "contain",
+  },
 });
 
-export default OnboardingScreen;
+export default () => {
+  return (
+    <SettingsProvider>
+      <OnboardingScreen />
+    </SettingsProvider>
+  );
+};
