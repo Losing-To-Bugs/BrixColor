a167694c39da32970f0680cb7c737d3ffedcf818
lh <pairofkings30@gmail.com>
2024-10-13 15:41:27 -0500

    Add simple tracking

--- src/components/VisionCamera.tsx
+++ src/components/VisionCamera.tsx
@@ -10,9 +10,43 @@ import {
 } from "react-native-vision-camera";
 import {Skia} from "@shopify/react-native-skia";
 import {detectBrick} from "@/hooks/detectBrick";
+import {useSharedValue} from "react-native-worklets-core";
 
 export type ScanCameraProps = CameraProps & {
     flashOn: boolean,
+}
+
+type Rect = [number, number, number, number]
+function IoU(box1: Rect, box2: Rect): number {
+    'worklet';
+    const [midX1, midY1, w1, h1] = box1
+    const [midX2, midY2, w2, h2] = box2
+
+    const minX1 = midX1 - w1/2
+    const minX2 = midX2 - w2/2
+    const minY1 = midY1 - h1/2
+    const minY2 = midY2 - h2/2
+
+    const maxX1 = midX1 + w1/2
+    const maxX2 = midX2 + w2/2
+    const maxY1 = midY1 + h1/2
+    const maxY2 = midY2 + h2/2
+
+    const inter_w = Math.min(maxX1, maxX2) - Math.max(minX1, minX2)
+    const inter_h = Math.min(maxY1, maxY2) - Math.max(minY1, minY2)
+
+    if (inter_w <= 0 || inter_h <= 0) return 0
+
+    const inter = inter_w * inter_h
+
+    const b1 = (maxX1 - minX1) * (maxY1 - minY1)
+    const b2 = (maxX2 - minX2) * (maxY2 - minY2)
+
+    const union = b1 + b2 - inter
+
+    const iou = inter / union
+
+    return iou
 }
 
 function getMaxPrediction(predictions: [number, number, number[]][]): [number, number, number[]] {
@@ -29,13 +63,23 @@ function getMaxPrediction(predictions: [number, number, number[]][]): [number, n
         }
     }
 
-    return predictions[maxIdx]
+    return [
+        predictions[maxIdx][0],
+        predictions[maxIdx][1],
+        [
+            predictions[maxIdx][2][0],
+            predictions[maxIdx][2][1],
+            predictions[maxIdx][2][2],
+            predictions[maxIdx][2][3],
+        ]
+    ]
 }
 
 const VisionCamera = forwardRef(function (props: ScanCameraProps, ref) {
     const device = useCameraDevice('back')
     const { hasPermission, requestPermission } = useCameraPermission()
     const [currentFrame, setCurrentFrame] = useState(null)
+    const tracking = useSharedValue<any>(null)
 
     const format = useCameraFormat(device, [
         // { videoResolution: { width: 1152, height: 640 } },
@@ -105,27 +149,80 @@ const VisionCamera = forwardRef(function (props: ScanCameraProps, ref) {
         const hScale = frame.height / 640
         const wScale = frame.width / 640
 
-        if (typeof result === 'object' && !Array.isArray(result)) {
+        if ((typeof result === 'object' && !Array.isArray(result)) || result?.length === 0) {
+            if (tracking.value !== null) {
+                let newScore = 0.8 * tracking.value.score
+                tracking.value = {
+                    x: tracking.value.x,
+                    y: tracking.value.y,
+                    width: tracking.value.width,
+                    height: tracking.value.height,
+                    score: newScore,
+                    label: tracking.value.label
+                }
+            }
+        } else {
+            const [maxClass, maxScore, rect] = getMaxPrediction(result)
+
+            if (maxScore < 0.7) {
+                if (tracking.value !== null) {
+                    let newScore = 0.8 * tracking.value.score
+                    tracking.value = {
+                        x: tracking.value.x,
+                        y: tracking.value.y,
+                        width: tracking.value.width,
+                        height: tracking.value.height,
+                        score: newScore,
+                        label: tracking.value.label
+                    }
+                }
+            } else {
+                const [x, y, width, height] = rect
+
+                if (tracking.value === null) {
+                    tracking.value = {
+                        x,
+                        y,
+                        width,
+                        height,
+                        score: maxScore,
+                        label: maxClass
+                    }
+                } else {
+                    const trackingRect = [tracking.value.x, tracking.value.y, tracking.value.width, tracking.value.height ]
+                    const iou = IoU(trackingRect, rect)
+
+                    let newScore = Math.min(1, 1 * tracking.value.score * (Math.min(iou, 0.75) + 0.5))
+
+                    if (iou < 0.1) {
+                        newScore = maxScore
+                    }
+
+                    tracking.value = {
+                        x,
+                        y,
+                        width,
+                        height,
+                        score: newScore,
+                        label: maxClass
+                    }
+                }
+            }
+        }
+
+        if (tracking.value === null) {
             frame.render()
             return
         }
 
-        if (result.length === 0) {
+        if (tracking.value.score < 0.1) {
             frame.render()
             return
         }
 
-        const [maxClass, maxScore, rect] = getMaxPrediction(result)
+        const {x, y, width, height} = tracking.value
 
-        if (maxScore < 0.7) {
-            frame.render()
-            return
-        }
-
-        console.log({ maxClass, maxLabel: labelMap[maxClass], maxScore, rect })
-
-        const [x, y, width, height] = rect
-
+        // Mid x, mid y, width, height
         const newRect = Skia.XYWHRect((x + width/2) * wScale, (y - height/2) * hScale, -width * wScale, height * hScale)
         const paint = Skia.Paint()
         paint.setColor(Skia.Color('rgba(255,0,0,0.18)'))
